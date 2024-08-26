a130928b2088c236ba4acc675b62a90e732a5778
Marina Bonin <marinacbonin@gmail.com>
2024-08-26 15:18:15 -0300

    Create remove item behavior

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+//switch modes
 function switchMode() {
   const html = document.documentElement;
   html.classList.toggle("dark");
 }
 
+//add new todo
 const input = document.getElementById("new-todo");
 const list = document.getElementById("list");
 function getTemplate(inputValue) {
@@ -32,17 +34,33 @@ function getTemplate(inputValue) {
             </span>`;
 }
 
+function removeItem(event) {
+  event.target.closest(".todo-list__item").remove();
+}
+
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     const inputValue = input.value;
+    console.log(event);
 
     if (inputValue.trim() !== "") {
       const listItem = document.createElement("div");
       listItem.classList.add("todo-list__item");
       listItem.innerHTML = getTemplate(inputValue);
+      listItem
+        .querySelector(".todo-list__item__delete")
+        .addEventListener("click", removeItem);
       list.prepend(listItem);
 
       input.value = "";
     }
   }
+});
+
+//remove todo
+const deleteElements = document.querySelectorAll(".todo-list__item__delete");
+console.log(deleteElements);
+
+deleteElements.forEach(function (deleteElem) {
+  deleteElem.addEventListener("click", removeItem);
 });
